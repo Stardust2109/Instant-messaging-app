65156a9ecee4d42071629fe4f0c553b5bbafb639
Stardust2109 <142626272+Stardust2109@users.noreply.github.com>
2024-08-24 02:00:17 +0530

    Update vite.config.js

--- chat-app/vite.config.js
+++ chat-app/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {
+      VITE_API_BASE_URL: process.env.VITE_API_BASE_URL || 'http://localhost:4040',
+    },
 })
